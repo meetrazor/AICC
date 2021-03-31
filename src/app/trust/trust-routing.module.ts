3699121c0f84957e0db3aabf38e318f61af36de9
meet trivedi <meetrazor1994@gmail.com>
2021-03-31 11:23:29 +0530

    check point

--- src/app/trust/trust-routing.module.ts
+++ src/app/trust/trust-routing.module.ts
@@ -5,11 +5,6 @@ import { SingleTrustViewComponent } from './single-trust-view/single-trust-view.
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ViewItReturnComponent } from './view-it-return/view-it-return.component';
-import { ViewExamCertificateComponent } from './view-exam-certificate/view-exam-certificate.component';
-import { ViewAuditComponent } from './view-audit/view-audit.component';
-import { ViewFundComponent } from './view-fund/view-fund.component';
-import { ViewMeetingComponent } from './view-meeting/view-meeting.component';
 
 const routes: Routes = [
   { path: 'view-trust/:trustID', component: SingleTrustViewComponent },
